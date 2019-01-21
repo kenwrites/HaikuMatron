2560749abdb6500a28a91cca5649a88242dbfda5
kenwrites <kenwrites@users.noreply.github.com>
2019-01-21 11:36:59 -0500

    Adding poem.js script

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-## Active URL
+## What's this?
+
+A website with silly haiku and limerick toys.  
+
+## URL
 
 Netlify:  https://festive-hopper-68bb10.netlify.com/
 
---
+++ js/poem.js
@@ -0,0 +1,1 @@
+document.write("Ever tardy was our man Nate<br>&apos;til he slipped and then broke his pate.<br>    It did not give us pause<br>when that hearse got lost:<br>to his own funeral he was late.");
