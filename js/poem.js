ba48499b112fc526f5589fba54a6dddc2828832b
kenwrites <kenwrites@users.noreply.github.com>
2019-01-24 09:34:02 -0500

    Adding script to print poem selected randomly from a list

--- js/poem.js
+++ js/poem.js
@@ -1,7 +1,10 @@
 var poem_array = [
         "Ever tardy was our man Nate<br>&apos;til he slipped and then broke his pate.<br>It did not give us pause<br>when that hearse got lost:<br>to his own funeral he was late.",
-        "Playing football is<br>not mandatory.  Therefore,<br>team named 'Volunteers.'"
-        "I once knew a Texan named Cora<br>who took her time walking (it’d bore ya’).<br>But one sad day her nose<br>stopped working – it froze.<br>No more stopping to smell the flora."
+        "Playing football is<br>not mandatory.  Therefore,<br>team named 'Volunteers.'",
+        "I once knew a Texan named Cora<br>who took her time walking (it’d bore ya’).<br>But one sad day her nose<br>stopped working – it froze.<br>No more stopping to smell the flora.",
+        "Noelle was a misplaced Californian.<br>In a Kentucky church she was born again.<br>But then out came the snakes<br>and worse, casserole bakes.<br>She was on the West Coast by the morn’ again.",
+        "Website has menu.<br>Why?  Is this the start of a<br>grand meal? Likely not.",
+        "'But why?' walker asks.<br>Runner shrugs, tightens laces,<br>stretches, and begins."
     ];
 var poem_index;
 var poem;
@@ -11,17 +14,14 @@ function print_poem(poem_html) {
     poem_box.innerHTML = poem_html;
 }
 
-function get_random_number(upper) {
+function get_random_index(upper) {
     var random_number = Math.floor(Math.random() * upper);
     return random_number;
 }
 
 // Select and print random poem
 
-poem_index = get_random_number(poem_array.length);
-console.log("poem_index: " + poem_index);
-
-poem = poem_array[1];
-
+poem_index = get_random_index(poem_array.length);
+poem = poem_array[poem_index];
 print_poem(poem);
 
