31afb702e7bd44df5cda337be43a29784b7019ed
kenwrites <kenwrites@users.noreply.github.com>
2019-01-24 14:48:14 -0500

    Adding wireframes and sitemaps

--- js/poem.js
+++ js/poem.js
@@ -9,6 +9,8 @@ var poem_array = [
 var poem_index;
 var poem;
 
+// Functions 
+
 function print_poem(poem_html) {
     var poem_box = document.getElementById("poem");
     poem_box.innerHTML = poem_html;
